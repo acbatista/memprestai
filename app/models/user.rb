69665a17e85812b43dbc1a292e6bc0a71093efef
Adriano Carvalho <beregueder@gmail.com>
2015-08-07 10:32:44 -0300

    implementação do model e controller user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :email, :full_name, :location, :password
   validates_confirmation_of :password
-  validates_lenght_of :bio, minimum: 30, allow_blank: false
-  validate_uniqueness_of :email
+  validates_length_of :bio, minimum: 30, allow_blank: false
+  validates_uniqueness_of :email
 
   validate :email_format
 
@@ -17,4 +17,3 @@ class User < ActiveRecord::Base
     end
   end
 
-end
